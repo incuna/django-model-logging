413a9231723db923479ad0e821c4bc4b48b65f20
Adam Thomas <sortoflikechess@gmail.com>
2015-10-06 12:46:47 +0100

    Reformat requirements a bit.

--- setup.py
+++ setup.py
@@ -4,6 +4,15 @@ from setuptools import find_packages, setup
 version = '0.1.0'
 
 
+extras_require={
+    'model_logging': [
+        'django-pgcrypto-fields>=1.0.0,<2.0.0',
+        'simplejson>=3.8.0,<4.0.0',
+        'djangorestframework>=3.2.4,<4.0.0',
+    ]
+}
+
+
 setup(
     name='django-model-logging',
     packages=find_packages(),
@@ -15,7 +24,7 @@ setup(
     author_email='admin@incuna.com',
     url='https://github.com/incuna/django-model-logging/',
     install_requires=[],
-    extras_require={},
+    extras_require=extras_require,
     zip_safe=False,
     license='BSD',
     classifiers=[
@@ -24,7 +33,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Testing',
     ],
 )
