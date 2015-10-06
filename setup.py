db4e27ba886e8f96eb9ee4743a9d3e1c72bae253
Adam Thomas <sortoflikechess@gmail.com>
2015-10-06 12:50:38 +0100

    Correct setup.py.

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ from setuptools import find_packages, setup
 version = '0.1.0'
 
 
-extras_require={
-    'model_logging': [
-        'django-pgcrypto-fields>=1.0.0,<2.0.0',
-        'simplejson>=3.8.0,<4.0.0',
-        'djangorestframework>=3.2.4,<4.0.0',
-    ]
+install_requires={
+    'django-pgcrypto-fields>=1.0.0,<2.0.0',
+    'simplejson>=3.8.0,<4.0.0',
+    'djangorestframework>=3.2.4,<4.0.0',
 }
 
 
@@ -23,8 +21,8 @@ setup(
     author='Incuna',
     author_email='admin@incuna.com',
     url='https://github.com/incuna/django-model-logging/',
-    install_requires=[],
-    extras_require=extras_require,
+    install_requires=install_requires,
+    extras_require={},
     zip_safe=False,
     license='BSD',
     classifiers=[
