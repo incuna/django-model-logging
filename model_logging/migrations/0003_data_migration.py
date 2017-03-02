8f75be79fab04494979ca5d12a2e7daebd60ae37
Ming Lee <ming@incuna.com>
2017-03-02 11:28:44 +0000

    Add backwards migration.

--- model_logging/migrations/0003_data_migration.py
+++ model_logging/migrations/0003_data_migration.py
@@ -16,6 +16,17 @@ def move_data(apps, schema_editor):
         entry.data_temp = entry.data
         entry.save()
 
+def move_data_back(apps, schema_editor):
+    try:
+        from pgcrypto.fields import TextPGPPublicKeyField
+    except ImportError:
+        raise ImportError('Please install django-pgcrypto-fields to perform migration')
+
+    LogEntry = apps.get_model(app, model)
+    for entry in LogEntry.objects.all():
+        entry.data = entry.data_temp
+        entry.save()
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -23,5 +34,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(move_data),
+        migrations.RunPython(move_data reverse_code=move_data_back),
     ]
